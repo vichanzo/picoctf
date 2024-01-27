a17eb4156af61de8c21226042c04f4690b1bf7b6
vichanzo <vichanzo@protonmail.com>
2024-01-27 19:40:24 +0000

    Completed 242 Glitch Cat

---
+++ 242glitchcat/runme.py
@@ -0,0 +1,8 @@
+
+import random
+
+
+flag = 'picoCTF{gl17ch_m3_n07_' + chr(0x39) + chr(0x63) + chr(0x34) + chr(0x32) + chr(0x61) + chr(0x34) + chr(0x35) + chr(0x64) + '}'
+
+print('Heres your flag: ' + flag)
+
